b845bdf130964edd99cd87c8c06e9205ffbc1425
SentientSam <46612325+SentientSam@users.noreply.github.com>
2019-09-19 09:31:53 -0500

    Create cstringdiff.h

---
+++ cstringdiff.h
@@ -0,0 +1,22 @@
+/*
+cstringdiff.h
+Project 6
+Samuel lamb
+cop3330
+4/1/2019
+*/
+
+#ifndef _CSTRINGDIFF_H
+#define _CSTRINGDIFF_H
+
+
+int LexDiff (const char* s1, const char* s2);
+
+int DictionaryDiff (const char* s1, const char* s2);
+
+bool LexLT (const char* s1, const char* s2);
+
+bool DictionaryLT (const char* s1, const char* s2);
+
+
+#endif
